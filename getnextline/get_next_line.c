38922f71c89a8590f592a9552540c8c892661b5d
Jonathan Novack <jnovack@c2r1s5.42luxembourg.lu>
2024-11-14 17:03:00 +0100

    add all files

--- getnextline/get_next_line.c
+++ getnextline/get_next_line.c
@@ -6,8 +6,79 @@
 /*   By: jnovack <jnovack@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 11:25:52 by jnovack           #+#    #+#             */
-/*   Updated: 2024/11/14 11:25:57 by jnovack          ###   ########.fr       */
+/*   Updated: 2024/11/14 17:00:06 by jnovack          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+static char	*get_line(char **buffer)
+{
+	char	*line;
+	char	*keep;
+	size_t	til_null;
+	size_t	til_new;
+
+	til_new = strlen_at(*buffer, '\n');
+	if((*buffer)[til_new] == '\n');
+		til_new++;
+	line = cpy_buffer(*buffer, til_new);
+	if (!line)
+		return (NULL);
+	til_null = strlen_at(*buffer, '\0');
+	keep = cpy_buffer(*buffer + til_new, til_null - til_new + 1);
+	if (!keep)
+	{
+		free (line);
+		return (NULL);
+	}
+	free(*buffer);
+	*buffer = keep;
+	return (line);
+}
+
+static char	*get_current_buffer(int fd, char *buffer)
+{
+	char	*current;
+	ssize_t	bytes;
+
+	bytes = 1;
+	current = (char *)malloc(BUFFER_SIZE + 1);
+	if (!current)
+		return (NULL);
+	while (bytes > 0 && !find_chr(buffer, '\n'))
+	{
+		bytes = read(fd, current, BUFFER_SIZE);
+		if (bytes == 0)
+			break ;
+		if (bytes == -1)
+		{
+			free(current);
+			return (NULL);
+		}
+		current[bytes] = '\0';
+		buffer = merge_previous_and_current(buffer, current);
+	}
+	free(current);
+	if (strlen_at(buffer, '\0') > 0)
+		return (buffer);
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = get_current_buffer(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = get_line(&buffer);
+	if (!buffer[0])
+	{
+		free (buffer);
+		buffer = NULL;
+	}
+	return (line);
--- getnextline/get_next_line.h
+++ getnextline/get_next_line.h
@@ -6,8 +6,31 @@
 /*   By: jnovack <jnovack@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 11:26:53 by jnovack           #+#    #+#             */
-/*   Updated: 2024/11/14 11:26:57 by jnovack          ###   ########.fr       */
+/*   Updated: 2024/11/14 16:42:15 by jnovack          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fnctl.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+size_t	strlen_at(const char *s, int c);
+char	*cpy_buffer(const char *s, size_t n);
+char	*find_chr(const char *s, int c);
+char	*merge_previous_and_current(char *s1, char const *s2);
+
+#endif
 
